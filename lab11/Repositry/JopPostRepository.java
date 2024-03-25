cddd07a0fe448b2404116cd206e948aa8d6f85aa
Reen00ad <105815655+Reen00ad@users.noreply.github.com>
2024-03-25 14:19:10 +0300

    Delete lab11 directory

--- lab11/Repositry/JopPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab11.Repositry;
-
-import com.example.lab11.Model.JopPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JopPostRepository extends JpaRepository<JopPost,Integer> {
-}
--- lab11/Repositry/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab11.Repositry;
-
-import com.example.lab11.Model.Userr;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Userr,Integer> {
-}
